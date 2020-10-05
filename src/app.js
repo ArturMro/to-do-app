7f8cedaf7947c35e129cd06f175e98e5b58e90e8
ArturM <artur.mroczkowski83@gmail.com>
2020-10-05 16:44:28 +0100

    fist todo app

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoItem from "./Todoitem.js";
+
+const TodoItemsFromOutSide = [
+    {id:"learn-react", labelName: "Learn react"},
+    {id:"create-todo-app", labelName: "Create a todo app"},
+    {id:"profit", labelName: "Profiiiit"},
+    {id:"have-fun", labelName:"Have fun!!"}
+]
+
+const App = () => (
+    <div id="my-todo-app" className="my-todo-app">
+        <h1>My todo app</h1>
+        
+        {TodoItemsFromOutSide.map((item) => <TodoItem key={item.id} id={item.id} labelName={item.labelName} />)}
+    </div>
+)
+
+ReactDOM.render(<App />, document.getElementById("root"))
