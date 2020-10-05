89b28ee3bd637238b100d0ab7dc92b5f79dd7ce9
ArturM <artur.mroczkowski83@gmail.com>
2020-10-05 19:14:41 +0100

    Add states

--- src/app.js
+++ src/app.js
@@ -1,20 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TodoItem from "./Todoitem.js";
+import React, { useState } from "react";
+import reactDOM from "react-dom";
 
-const TodoItemsFromOutSide = [
-    {id:"learn-react", labelName: "Learn react"},
-    {id:"create-todo-app", labelName: "Create a todo app"},
-    {id:"profit", labelName: "Profiiiit"},
-    {id:"have-fun", labelName:"Have fun!!"}
-]
+import TodoItem from "./TodoItem";
 
-const App = () => (
+
+const TodoItemsFromOutside = [
+  { id: "learn-react", labelName: "Learn React" },
+  { id: "create-todo-app", labelName: "Create a todo app" },
+  { id: "profit", labelName: "Profit" },
+  { id: "prettier", labelName: "Prettier is awesome!" },
+];
+
+const App = () => {
+  const [todos, setTodos] = useState(TodoItemsFromOutside);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodos = [
+      ...todos,
+      { id: newTodo.replace(" ", "-"), labelName: newTodo },
+    ];
+    setTodos(newTodos);
+    setNewTodo("");
+  };
+
+  return (
     <div id="my-todo-app" className="my-todo-app">
-        <h1>My todo app</h1>
-        
-        {TodoItemsFromOutSide.map((item) => <TodoItem key={item.id} id={item.id} labelName={item.labelName} />)}
+      <h1>My todo app</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          value={newTodo}
+          onChange={(e) => {
+            setNewTodo(e.target.value);
+          }}
+        />
+      </form>
+      {todos.map((item) => {
+        return (
+          <TodoItem key={item.id} id={item.id} labelName={item.labelName} />
+        );
+      })}
     </div>
-)
-
-ReactDOM.render(<App />, document.getElementById("root"))
+  );
+};
+// React.createElement(App) == <app />
+reactDOM.render(<App />, document.getElementById("root"));
